38c0488f6750412885b80c4e3d8d955005e515be
z935642417 <935642417@qq.com>
2026-05-02 23:30:01 +0800

    feat: complete package imports for MVP

--- src/dvb_parser/__init__.py
+++ src/dvb_parser/__init__.py
@@ -5,14 +5,25 @@ DVB Parser - DVB protocol parser for satellite signal analysis
 __version__ = "0.1.0"
 
 from dvb_parser.bbframe.parser import BBFrameParser
-from dvb_parser.bbframe.models import BBFrame, BBFrameHeader
+from dvb_parser.bbframe.models import BBFrame, BBFrameHeader, StreamType
 from dvb_parser.ts.parser import TSPacketParser
-from dvb_parser.ts.models import TSPacket
+from dvb_parser.ts.models import TSPacket, AdaptationField
+from dvb_parser.psi.pat import PATParser
+from dvb_parser.psi.pmt import PMTParser
+from dvb_parser.psi.models import PAT, PMT, PATEntry, PMTStream
 
 __all__ = [
     "BBFrameParser",
     "BBFrame",
     "BBFrameHeader",
+    "StreamType",
     "TSPacketParser",
     "TSPacket",
+    "AdaptationField",
+    "PATParser",
+    "PMTParser",
+    "PAT",
+    "PMT",
+    "PATEntry",
+    "PMTStream",
 ]
--- src/dvb_parser/psi/__init__.py
+++ src/dvb_parser/psi/__init__.py
@@ -2,5 +2,13 @@
 
 from dvb_parser.psi.pat import PATParser
 from dvb_parser.psi.pmt import PMTParser
+from dvb_parser.psi.models import PAT, PMT, PATEntry, PMTStream
 
-__all__ = ['PATParser', 'PMTParser']
+__all__ = [
+    "PATParser",
+    "PMTParser",
+    "PAT",
+    "PMT",
+    "PATEntry",
+    "PMTStream",
+]
